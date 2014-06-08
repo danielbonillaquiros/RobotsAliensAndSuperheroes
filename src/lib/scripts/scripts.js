717a0b874aefba0f65c856d70957eee6cf45e6ca
MeDicenPilon <josepablofg@gmail.com>
2014-06-08 05:10:26 -0600

    Holi canguro

---
+++ src/lib/scripts/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$(".results").hide();
+	$('.details').hide();
+	$('.itemGO').click(function(){
+		$(".results").show();
+	});
+		
+	$('.item').click(function(){
+		if($("#expcol", this).text() == "v"){
+			$("#expcol", this).text(">");
+		}
+		else{
+			$("#expcol", this).text("v");
+		}
+	});
+	
+	$('#item3').click(function(){
+		$(".details").show();
+	});
+});
